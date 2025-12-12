56fd088a19b7f7ee14953412b30cca71d9b8e630
Your Name <you@example.com>
2025-12-12 11:01:54 +0100

    cezar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,26 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(szyfrowanie("zsa" ,3));
+
     }
-    public String szyfrowanie(){
-        String zaszyfrowane = "";
-        return zaszyfrowane;
+    public static String szyfrowanie(String tekst, int klucz){
+        String alfabet = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder zaszyfrowana = new StringBuilder();
+        klucz = ((klucz % 26) + 26) % 26;
+        System.out.println(klucz);
+        for (int i = 0; i < tekst.length(); i++) {
+            char c = tekst.charAt(i);
+            if (c == ' ') {
+                zaszyfrowana.append(' ');
+                continue;
+            }
+            int index = alfabet.indexOf(c);
+            if (index != -1) {
+                int nowyIndex = (index + klucz) % 26;
+                zaszyfrowana.append(alfabet.charAt(nowyIndex));
+            }
+        }
+        return zaszyfrowana.toString();
     }
 }
