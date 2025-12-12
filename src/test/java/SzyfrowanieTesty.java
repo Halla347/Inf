1d7d5bcb65e81c971afe3ea66bc901bf436c4346
Your Name <you@example.com>
2025-12-12 12:10:38 +0100

    testy tworzenie pierwsyz test

--- src/test/java/SzyfrowanieTesty.java
+++ src/test/java/SzyfrowanieTesty.java
@@ -1,6 +1,9 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
+import org.example.Main;
 public class SzyfrowanieTesty {
     @Test
+    public void DanePodstawowe(){
 
+    }
 }
