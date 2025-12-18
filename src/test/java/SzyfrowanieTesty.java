43e9677c3028c62d37c963e464d494e18018fca9
Kacper <foksinskikacper@gmail.com>
2025-12-18 20:39:50 +0100

    testy dziala

--- src/test/java/SzyfrowanieTesty.java
+++ src/test/java/SzyfrowanieTesty.java
@@ -4,6 +4,10 @@ import org.example.Main;
 public class SzyfrowanieTesty {
     @Test
     public void DanePodstawowe(){
-
+        Assertions.assertEquals("def",Main.szyfruj("abc",3));
+        Assertions.assertEquals("xyz",Main.szyfruj("abc",-3));
+        Assertions.assertEquals("def",Main.szyfruj("abc",-23));
+        Assertions.assertEquals("xyz",Main.szyfruj("abc",23));
+        Assertions.assertEquals("abc",Main.szyfruj("abc",26));
     }
 }
